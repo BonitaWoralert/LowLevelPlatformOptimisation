3fbbe82a5763c47e4cabed8c3b3b518aa1b001c3
Bonita Woralert <67742281+BonitaWoralert@users.noreply.github.com>
2023-11-13 11:35:32 +0000

    Implemented a check value in header and footer

--- MemAlloc.cpp
+++ MemAlloc.cpp
@@ -8,11 +8,12 @@ struct Header
 {
 	int size; //size of main allocated section
 	MemoryTracker* tracker; //memory tracker used
+	int checkValue;
 };
 
 struct Footer
 {
-	int reserved;
+	int checkValue;
 };
 
 void* operator new (size_t size)
@@ -24,11 +25,16 @@ void* operator new (size_t size)
 	Header* pHeader = (Header*)pMem; //header pointer = start of allocated memory
 
 	pHeader->size = size; //set size int to the same as size passed into new
-	pHeader->tracker = defaultTracker;
-	pHeader->tracker->AddBytesAllocated(size);
+	pHeader->tracker = defaultTracker; //set memory tracker
+	pHeader->tracker->AddBytesAllocated(size); //add bytes to memory tracker
+
+	pHeader->checkValue = 0x4EADC0DE; //set header check value
+
 	std::cout << "\nBytes requested: " << size << "\t\tTotal Allocated Bytes = " << pHeader->tracker->GetAllocated();
 	void* pFooterAddr = pMem + sizeof(Header) + size; //pointer to footer (start address + header + requested bytes)
 	Footer* pFooter = (Footer*)pFooterAddr; //footer pointer = end
+
+	pFooter->checkValue = 0xF007C0DE; //set footer check value
 
 	std::cout << "\nHeader address: " << &pHeader << "\tFooter Address: " << &pFooter;
 
@@ -43,7 +49,20 @@ void operator delete (void* pMem)
 	Header* pHeader = (Header*)((char*)pMem - sizeof(Header)); //header = sizeof(Header) bytes before start
 	Footer* pFooter = (Footer*)((char*)pMem + pHeader->size); //footer
 
-	pHeader->tracker->RemoveBytesAllocated(pHeader->size);
-	std::cout << "\nBytes deleted: " << pHeader->size << "\t\tTotal Allocated Bytes = " << pHeader->tracker->GetAllocated();
-	free(pHeader);
+	if (pHeader->checkValue == 0x4EADC0DE && pFooter->checkValue == 0xF007C0DE)
+	{
+		pHeader->tracker->RemoveBytesAllocated(pHeader->size);
+		std::cout << "\nBytes deleted: " << pHeader->size << "\t\tTotal Allocated Bytes = " << pHeader->tracker->GetAllocated();
+		free(pHeader);
+	}
+	else
+	{
+		if (pHeader->checkValue != 0x4EADC0DE && pFooter->checkValue != 0xF007C0DE)
+			std::cout << "\n\nCheck values incorrect in header and footer.\n\n";
+		else if (pHeader->checkValue != 0x4EADC0DE)
+			std::cout << "\n\nCheck values incorrect in header.\n\n";
+		else
+			std::cout << "\n\nCheck values incorrect in footer.\n\n";
+	}
+
 }
